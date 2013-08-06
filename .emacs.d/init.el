6500e97e5224d08c75aa654542b00037dbb9e51c
Dane Powell <git@danepowell.com>
2013-08-06 15:55:12 -0500

    Added emacs dotfiles.

---
+++ .emacs.d/init.el
@@ -0,0 +1,17 @@
+(add-to-list 'load-path "~/.emacs.d")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(setq
+  backup-by-copying t
+  backup-directory-alist `(("." . "~/.saves")))
+(setq auto-save-file-name-transforms
+          `((".*" ,temporary-file-directory t)))
+(setq abbrev-file-name "~/.emacs.d/abbrev_defs")
+
+(require 'sgml-mode)
+(add-to-list 'auto-mode-alist '("\\.launch\\'" . sgml-mode))
+
