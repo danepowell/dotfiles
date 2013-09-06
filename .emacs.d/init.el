816d2691f108dd7dff752296610f499e3ecd8aea
Dane Powell <git@danepowell.com>
2013-09-06 09:30:36 -0500

    Added clean-boot alias.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,10 +25,11 @@
 ;; Require modes
 (require 'sgml-mode)
 (require 'drupal-mode)
+(require 'markdown-mode)
 
 ;; File associations
 (add-to-list 'auto-mode-alist '("\\.launch\\'" . sgml-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
-
+(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
--- bash/.bash_aliases
+++ bash/.bash_aliases
@@ -4,6 +4,7 @@ alias e='emacs -nw'
 if [ $UID -ne 0 ]; then
   alias reboot-delay='sudo shutdown -r 04:00 "Server will reboot overnight." &'
   alias wifi-scan='sudo iwlist wlan0 scan'
+  alias clean-boot='dpkg --get-selections|grep '"'"'linux-image*'"'"'|awk '"'"'{print $1}'"'"'|egrep -v "linux-image-$(uname -r)|linux-image-generic" |while read n;do sudo apt-get -y remove $n;done'
 fi
 
 if [ -f ~/.bash_aliases.local ]; then
