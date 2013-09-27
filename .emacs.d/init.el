fe968f051f52a4b0348e27c6e3cbee979627aafc
Dane Powell <git@danepowell.com>
2013-09-27 14:20:55 -0500

    Edit all PHP files in drupal mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,6 @@
 ;; File associations
 (add-to-list 'auto-mode-alist '("\\.launch\\'" . sgml-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
-(add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . drupal-mode))
 (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
