ce81d10b669aefe7e7ea743cad424d826a6485f6
Dane Powell <git@danepowell.com>
2023-08-10 09:00:29 -0700

    add zoom, fix corepack

--- macos/dock.sh
+++ macos/dock.sh
@@ -6,5 +6,6 @@ dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Klipped.app"
 dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Zoom.us.app"
 
 killall Dock
