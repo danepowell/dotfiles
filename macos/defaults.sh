3aeaa6abde0fe82c37fa30c453e25b13c44fccf1
Dane Powell <git@danepowell.com>
2023-12-20 11:29:54 -0800

    manage iterm2 prefs

---
+++ macos/defaults.sh
@@ -0,0 +1,63 @@
+COMPUTER_NAME="Neo"
+
+# Topics
+#
+# - Computer & Host name
+# - Localization
+# - System
+# - Keyboard & Input
+# - Trackpad, mouse, Bluetooth accessories
+# - Screen
+# - Finder
+# - Dock
+# - Mail
+# - Calendar
+# - Terminal
+# - Activity Monitor
+# - Software Updates
+
+osascript -e 'tell application "System Preferences" to quit'
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until this script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###############################################################################
+# Computer & Host name                                                        #
+###############################################################################
+
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName "$COMPUTER_NAME"
+sudo scutil --set HostName "$COMPUTER_NAME"
+sudo scutil --set LocalHostName "$COMPUTER_NAME"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$COMPUTER_NAME"
+
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
+# Disable hot corners
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
+
+# Don't show recently used applications in the Dock
+defaults write com.Apple.Dock show-recents -bool false
+
+###############################################################################
+# iTerm2                                                                        #
+###############################################################################
+
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string "$HOME/.config/iterm2"
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Address Book" "Calendar" "Contacts" "Dock" "Finder" "Mail" "Safari" "SystemUIServer" "iCal"; do
+  killall "${app}" &> /dev/null
+done
