055901493178ad4cdb8429adb8556caeb0878dae
Dane Powell <git@danepowell.com>
2024-03-22 09:25:05 -0700

    Ubuntu defaults

---
+++ ubuntu/defaults.sh
@@ -0,0 +1,2 @@
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary>Tab'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Tab'
